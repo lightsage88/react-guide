e5d5f50cdd51e63bc2df0662ce6ddbbf711a9f63
Adrian <adrian.e.rosales@gmail.com>
2019-11-02 12:59:50 -0700

    we have converted Clock into a class-component so we can later include a timer and the updating of the UI itself via state

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function Clock(props) {
-    return (
-        <div>
-            <h1>Hello World!</h1>
-            <h2>It is {props.date.toLocaleTimeString()}</h2>
-        </div>
-    );
+
+class Clock extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is, {this.props.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
 }
 
 
