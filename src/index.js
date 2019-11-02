5c88b448683558e0156b0d275b47d9b3985cf4b8
Adrian <adrian.e.rosales@gmail.com>
2019-11-02 12:57:44 -0700

    we have encapsulated the clock into its own function-component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function Clock(props) {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <h2>It is {props.date.toLocaleTimeString()}</h2>
+        </div>
+    );
+}
+
+
 function tick() {
     const element = (
         <div>
