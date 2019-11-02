29ce811ce0def63b7d60c82f146ddd390a7bb43b
Adrian <adrian.e.rosales@gmail.com>
2019-11-02 13:23:44 -0700

    just a demonstrataion of the independence of components by showing App with 3 Clock components in it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
@@ -56,9 +56,17 @@ class Clock extends React.Component {
 }
 
 
+function App() {
+    return (
+        <div>
+            <Clock />
+            <Clock />
+            <Clock />
+        </div>
+    )
+}
 
-
-ReactDOM.render(<Clock />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
