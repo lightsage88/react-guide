ffbd1694fa9cfc79711217aa6c496396509766a4
Adrian <adrian.e.rosales@gmail.com>
2019-11-02 15:58:29 -0700

    created a form which updates the inputs value by firing an element-binded onChange method which updates the state of the FormComponent...this is a controlled component. We also prevent autoreload with the event argument which is passed from the element-binding on onSubmit

--- src/index.js
+++ src/index.js
@@ -197,9 +197,46 @@ function App() {
             <LoginControl />
             <Mailbox messages={messages}/>
             <NumberList numbers={numbers} />
+            <NameFormControlledComponent />
         </div>
     )
 }
+
+class NameFormControlledComponent extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            value: ''
+        };
+    }
+
+    handleChange(event) {
+        this.setState({
+            value: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        console.log(this.state.value);
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit.bind(this)}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange.bind(this)} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        )
+    }
+}
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
